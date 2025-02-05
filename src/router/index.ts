3da709f91ddc9d7cacc33f72ba9848a2b620dea1
AbdouRGitHub <94540594+AbdouRGitHub@users.noreply.github.com>
2025-02-05 13:40:29 +0100

    feat: initial project

---
+++ src/router/index.ts
@@ -0,0 +1,11 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', component: import('@/views/Home.vue') },
+    { path: '/#', component: () => import('@/views/Register.vue') },
+  ],
+})
+
+export default router
---
+++ src/views/Home.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <h1>Home</h1>
+    <p>Welcome to your new project!</p>
+  </div>
+</template>
+
+<style scoped></style>
