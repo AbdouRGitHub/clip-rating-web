2d7c929c3fe07364197862fa987abf27ff18e7ce
AbdouRGitHub <94540594+AbdouRGitHub@users.noreply.github.com>
2025-02-13 22:20:33 +0100

    feat: update router configuration to replace Register.vue with Login.vue and adjust Home.vue path

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,11 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/', component: import('@/views/Home.vue') },
-    { path: '/#', component: () => import('@/views/Register.vue') },
+    { path: '/', component: () => import('../views/Login.vue') },
+    { path: '/home', component: () => import('../views/Home.vue') },
   ],
-})
+});
 
-export default router
+export default router;
