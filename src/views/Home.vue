308d53aefc2e590a24f6fa80d5eb91eacfc8d1f5
AbdouRGitHub <94540594+AbdouRGitHub@users.noreply.github.com>
2025-02-13 22:04:13 +0100

    feat: refactor authentication views by removing Home.vue, adding Login.vue, and deleting Register.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,66 +1,7 @@
-<script setup lang="ts">
-import { reactive } from 'vue';
-import BaseInput from '@/components/input/BaseInput.vue';
-import BaseButton from '@/components/button/BaseButton.vue';
-
-const formData = reactive<{ email: string; password: string }>({
-  email: '',
-  password: '',
-});
-
-const submitForm: () => void = () => {};
-</script>
+<script lang="ts"></script>
 
 <template>
-  <div class="home">
-    <div class="login-container">
-      <form @submit.prevent="submitForm">
-        <BaseInput type="email" placeholder="Email" v-model="formData.email" />
-        <BaseInput type="password" placeholder="Password" v-model="formData.password" />
-        <BaseButton type="submit">Login</BaseButton>
-      </form>
-      <a> forget your password ? </a>
-    </div>
-  </div>
+  <h1>Home</h1>
 </template>
 
-<style scoped>
-.home {
-  display: flex;
-  width: 100%;
-  height: 100vh;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  row-gap: 20px;
-}
-
-.login-container {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  row-gap: 5px;
-  padding: 20px;
-  width: 400px;
-  height: 500px;
-  background-color: var(--color-box-primary);
-  border-radius: 2%;
-}
-
-.login-container form {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  row-gap: 5px;
-  width: 100%;
-  height: 100%;
-}
-
-button {
-  width: 150px;
-  height: 50px;
-  padding: 2px;
-}
-</style>
+<style scoped></style>
---
+++ src/views/Login.vue
@@ -0,0 +1,110 @@
+<script setup lang="ts">
+import { reactive, ref } from 'vue';
+import BaseInput from '@/components/input/BaseInput.vue';
+import BaseButton from '@/components/button/BaseButton.vue';
+import { login } from '@/services/auth';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+const formData = reactive<{ email: string; password: string }>({
+  email: '',
+  password: '',
+});
+
+const errors = reactive<{ email: string | null; password: string | null }>({
+  email: null,
+  password: null,
+});
+
+const serverError = ref<string>();
+
+const validateEmail = (email: string): boolean => {
+  if (!email) {
+    errors.email = 'Email is required';
+    return false;
+  }
+  errors.email = null;
+  return true;
+};
+
+const validatePassword = (password: string): boolean => {
+  if (!password) {
+    errors.password = 'Password is required';
+    return false;
+  }
+  errors.password = null;
+  return true;
+};
+
+const submitForm = async () => {
+  const isEmailValid = validateEmail(formData.email);
+  const isPasswordValid = validatePassword(formData.password);
+
+  if (isEmailValid && isPasswordValid) {
+    const response: { success: boolean; message?: string } = await login(
+      formData.email,
+      formData.password,
+    );
+
+    if (response.success == true) {
+      // Redirect to dashboard
+    } else {
+      serverError.value = response.message;
+    }
+  }
+};
+</script>
+
+<template>
+  <div class="login">
+    <div class="login-container">
+      <span v-if="serverError" class="error-message">{{ serverError }}</span>
+      <form @submit.prevent="submitForm">
+        <BaseInput type="email" placeholder="Email" v-model="formData.email" />
+        <span v-if="errors.email" class="error-message">{{ errors.email }}</span>
+        <BaseInput type="password" placeholder="Password" v-model="formData.password" />
+        <span v-if="errors.password" class="error-message">{{ errors.password }}</span>
+        <BaseButton type="submit">Login</BaseButton>
+      </form>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.login {
+  display: flex;
+  width: 100%;
+  height: 100vh;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  row-gap: 20px;
+}
+
+.login-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  row-gap: 5px;
+  padding: 20px;
+  width: 400px;
+  height: 500px;
+  background-color: var(--color-box-primary);
+  border-radius: 2%;
+}
+
+.login-container form {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  row-gap: 5px;
+  width: 100%;
+  height: 100%;
+}
+
+.error-message {
+  color: var(--color-text-danger);
+}
+</style>
