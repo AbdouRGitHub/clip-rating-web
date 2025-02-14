3ab7a5eabfff9b68e026e6975c385ca86448c6b8
AbdouRGitHub <94540594+AbdouRGitHub@users.noreply.github.com>
2025-02-14 19:29:49 +0100

    feat: implement route guard for authentication on protected routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,23 @@
+import { checkSession } from '@/services/auth';
 import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/', component: () => import('../views/Login.vue') },
-    { path: '/home', component: () => import('../views/Home.vue') },
+    { path: '/', component: () => import('@/views/Login.vue') },
+    { path: '/home', component: () => import('@/views/Home.vue'), meta: { requiresAuth: true } },
   ],
+});
+
+router.beforeEach(async (to, from, next) => {
+  if (to.meta.requiresAuth) {
+    const isAuthenticated: boolean = await checkSession();
+    if (isAuthenticated) {
+      next();
+    } else {
+      next('/');
+    }
+  }
 });
 
 export default router;
