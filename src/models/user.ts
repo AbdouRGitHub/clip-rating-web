3ddd2feb96b06393460276a53211cb92235ca8cb
AbdouRGitHub <94540594+AbdouRGitHub@users.noreply.github.com>
2025-02-17 16:37:40 +0100

    feat: add User interface model

---
+++ src/models/user.ts
@@ -0,0 +1,10 @@
+export default interface User {
+  id: string;
+  email?: string;
+  username?: string;
+  password?: string;
+  avatar_path?: string;
+  role?: string;
+  createdAt?: string;
+  updatedAt?: string;
+}
