6d87eb43e9d6bbb21ee8ed2091c58834a7eac25a
AbdouRGitHub <94540594+AbdouRGitHub@users.noreply.github.com>
2025-02-18 16:55:55 +0100

    style: modify Login.vue to set height to 100% for flexible layout

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -75,7 +75,7 @@ const submitForm = async () => {
 .login {
   display: flex;
   width: 100%;
-  height: 100vh;
+  height: 100%;
   flex-direction: column;
   justify-content: center;
   align-items: center;
